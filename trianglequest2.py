9909c0a8b2947cae4ac475dc013510df89fe74a7
adilahiri <lahiri@aggienetwork.com>
2018-02-06 23:03:56 -0600

    Triangle Quest 2

---
+++ trianglequest2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 23:00:32 2018
+
+@author: adi44
+"""
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i//9)**2)
