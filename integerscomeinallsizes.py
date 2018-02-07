7f5fefb47f9ab1cb7be8bfd7a14665030a0d35da
adilahiri <lahiri@aggienetwork.com>
2018-02-06 23:45:48 -0600

    integers come in all sizes

---
+++ integerscomeinallsizes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 23:44:32 2018
+
+@author: adi44
+"""
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(pow(a,b) + pow(c,d))
